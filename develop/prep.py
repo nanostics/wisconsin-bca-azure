d74c90406a10c73fafe0af2252d9fd5ac1657885
Joshua Ji <joshua.ji@jiwhiz.com>
2023-05-19 15:36:43 +0000

    Update prep.py

--- develop/prep.py
+++ develop/prep.py
@@ -7,38 +7,43 @@ Prepares raw data and provides training, validation and test datasets
 import argparse
 
 from pathlib import Path
-import os
 import numpy as np
 import pandas as pd
 
 import mlflow
 
-TARGET_COL = "cost"
+TARGET_COL = "diagnosis_01"
 
 NUMERIC_COLS = [
-    "distance",
-    "dropoff_latitude",
-    "dropoff_longitude",
-    "passengers",
-    "pickup_latitude",
-    "pickup_longitude",
-    "pickup_weekday",
-    "pickup_month",
-    "pickup_monthday",
-    "pickup_hour",
-    "pickup_minute",
-    "pickup_second",
-    "dropoff_weekday",
-    "dropoff_month",
-    "dropoff_monthday",
-    "dropoff_hour",
-    "dropoff_minute",
-    "dropoff_second",
+    'texture_mean',
+    'smoothness_mean',
+    'compactness_mean',
+    'concavity_mean',
+    'concave points_mean',
+    'symmetry_mean',
+    'fractal_dimension_mean',
+    'radius_se',
+    'texture_se',
+    'perimeter_se',
+    'area_se',
+    'smoothness_se',
+    'compactness_se',
+    'concavity_se',
+    'concave points_se',
+    'symmetry_se',
+    'fractal_dimension_se',
+    'texture_worst',
+    'perimeter_worst',
+    'area_worst',
+    'smoothness_worst',
+    'compactness_worst',
+    'concavity_worst',
+    'concave points_worst',
+    'symmetry_worst',
+    'fractal_dimension_worst'
 ]
 
 CAT_NOM_COLS = [
-    "store_forward",
-    "vendor",
 ]
 
 CAT_ORD_COLS = [
@@ -52,18 +57,9 @@ def parse_args():
     parser.add_argument("--train_data", type=str, help="Path to train dataset")
     parser.add_argument("--val_data", type=str, help="Path to test dataset")
     parser.add_argument("--test_data", type=str, help="Path to test dataset")
-    
-    parser.add_argument("--enable_monitoring", type=str, help="enable logging to ADX")
-    parser.add_argument("--table_name", type=str, default="mlmonitoring", help="Table name in ADX for logging")
-    
-    args = parser.parse_args()
 
-    return args
+    return parser.parse_args()
 
-def log_training_data(df, table_name):
-    from obs.collector import Online_Collector
-    collector = Online_Collector(table_name)
-    collector.batch_collect(df)
 
 def main(args):
     '''Read, split, and save datasets'''
@@ -71,7 +67,7 @@ def main(args):
     # ------------ Reading Data ------------ #
     # -------------------------------------- #
 
-    data = pd.read_csv((Path(args.raw_data)))
+    data = pd.read_parquet((Path(args.raw_data)))
     data = data[NUMERIC_COLS + CAT_NOM_COLS + CAT_ORD_COLS + [TARGET_COL]]
 
     # ------------- Split Data ------------- #
@@ -81,24 +77,19 @@ def main(args):
 
     random_data = np.random.rand(len(data))
 
+    # for
     msk_train = random_data < 0.7
-    msk_val = (random_data >= 0.7) & (random_data < 0.85)
-    msk_test = random_data >= 0.85
+    msk_test = random_data >= 0.7
 
     train = data[msk_train]
-    val = data[msk_val]
     test = data[msk_test]
 
     mlflow.log_metric('train size', train.shape[0])
-    mlflow.log_metric('val size', val.shape[0])
     mlflow.log_metric('test size', test.shape[0])
 
     train.to_parquet((Path(args.train_data) / "train.parquet"))
-    val.to_parquet((Path(args.val_data) / "val.parquet"))
     test.to_parquet((Path(args.test_data) / "test.parquet"))
 
-    if (args.enable_monitoring.lower() == 'true' or args.enable_monitoring == '1' or args.enable_monitoring.lower() == 'yes'):
-        log_training_data(data, args.table_name)
 
 
 if __name__ == "__main__":
@@ -120,7 +111,7 @@ if __name__ == "__main__":
 
     for line in lines:
         print(line)
-    
+
     main(args)
 
     mlflow.end_run()
