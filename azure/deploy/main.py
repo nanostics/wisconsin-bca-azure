83422b2f10151c6c198bf35835e1432187682887
Joshua Ji <joshua.ji@jiwhiz.com>
2023-05-24 19:25:45 +0000

    Rename `create_ml_environment` -> `ml_environment`

--- azure/deploy/main.py
+++ azure/deploy/main.py
@@ -4,7 +4,7 @@ import deploy_helpers as helper
 if __name__ == '__main__':
     mlclient = helper.get_mlclient()
     model = helper.get_latest_model(mlclient, local=False)
-    environment = helper.create_ml_environment(local=False)
+    environment = helper.ml_environment(local=False)
     endpoint = helper.create_or_update_endpoint(mlclient, local=False)
     helper.create_or_update_deployment(mlclient, model, environment, endpoint, local=False)
 
