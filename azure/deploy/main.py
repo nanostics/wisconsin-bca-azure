3a084a8bf9b59f33563374bc709c0f826540da8b
Joshua Ji <joshua.ji@jiwhiz.com>
2023-05-24 21:24:48 +0000

    Fix `ml_environment` usage

--- azure/deploy/main.py
+++ azure/deploy/main.py
@@ -4,7 +4,7 @@ import deploy_helpers as helper
 if __name__ == '__main__':
     mlclient = helper.get_mlclient()
     model = helper.get_latest_model(mlclient, local=False)
-    environment = helper.ml_environment(local=False)
+    environment = helper.ml_environment(mlclient, local=False)
     endpoint = helper.create_or_update_endpoint(mlclient, local=False)
     deployment_name = helper.create_or_update_deployment(mlclient, model, environment, endpoint, local=False)
 
