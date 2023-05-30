6d18ab27e2a5c42238732c1336268460583f21b2
Joshua Ji <joshua.ji@jiwhiz.com>
2023-05-30 20:03:17 +0000

    Move `prep` component to `lib/pipeline`

--- azure/pipeline/main.py
+++ azure/pipeline/main.py
@@ -4,9 +4,28 @@
 # relative imports from lib/deploy_helpers.py
 import deploy_helpers as helper
 
+from pipeline.prep import prepare_data_component
+
+from azure.ai.ml import Input
+from azure.ai.ml.dsl import pipeline
+
+# define a pipeline
+@pipeline(
+    default_compute='cpu-cluster',
+)
+def wisconsin_bca_pipeline(input_data):
+    '''
+    E2E ML pipeline using the Wisconsin BCa Dataset
+    '''
+    prep_node = prepare_data_component(raw_data=input_data)
+
+
 # ref: https://learn.microsoft.com/en-us/azure/machine-learning/how-to-create-component-pipeline-python?view=azureml-api-2
 if __name__ == '__main__':
     client = helper.get_mlclient()
-    
 
-    print('Authenticated!')
+    help(prepare_data_component)
+    
+    # create a pipeline
+    wisconsin_bca_dataset = Input(type='uri_file', path='azureml:wisconsin-bca-data@latest')
+    pipeline_job = wisconsin_bca_pipeline(input_data=wisconsin_bca_dataset)
---
+++ lib/pipeline/prep.py
@@ -0,0 +1,73 @@
+'''
+Prepares raw data and provides training, validation and test datasets
+
+See also: https://github.com/Azure/mlops-v2-gha-demo/blob/bd61f8c79c7c4cf4e30fe83a88e2b0532996ef33/data-science/src/prep.py
+'''
+
+from pathlib import Path
+from mldesigner import command_component, Input, Output
+import mlflow
+from  pipeline import constants
+import numpy as np
+import pandas as pd
+
+
+@command_component(
+    name="prep_data",
+    version="1",
+    display_name="Prep Data",
+    description="Prepares raw data and provides training, validation and test datasets",
+    environment="azureml:wisconsin-bca-env@latest"
+)
+def prepare_data_component(
+    raw_data: Input(type="uri_file"),
+    train_data: Output(type="uri_folder"),
+    val_data: Output(type="uri_folder"),
+    test_data: Output(type="uri_folder")
+):
+    '''
+    A code-version of what was in `pipeline.yml`
+    '''
+    mlflow.start_run()
+
+    # ----------  Arguments ----------- #
+    # --------------------------------- #
+
+    lines = [
+        f"Raw data path: {raw_data}",
+        f"Train dataset output path: {train_data}",
+        f"Val dataset output path: {val_data}",
+        f"Test dataset path: {test_data}",
+
+    ]
+
+    for line in lines:
+        print(line)
+
+    # ------------ Reading Data ------------ #
+    # -------------------------------------- #
+
+    data = pd.read_parquet((Path(raw_data)))
+    data = data[constants.NUMERIC_COLS + constants.CAT_NOM_COLS + constants.CAT_ORD_COLS + [constants.TARGET_COL]]
+
+    # ------------- Split Data ------------- #
+    # -------------------------------------- #
+
+    # Split data into train, val and test datasets
+
+    random_data = np.random.rand(len(data))
+
+    # for
+    msk_train = random_data < 0.7
+    msk_test = random_data >= 0.7
+
+    train = data[msk_train]
+    test = data[msk_test]
+
+    mlflow.log_metric('train size', train.shape[0])
+    mlflow.log_metric('test size', test.shape[0])
+
+    train.to_parquet((Path(train_data) / "train.parquet"))
+    test.to_parquet((Path(test_data) / "test.parquet"))
+
+    mlflow.end_run()
