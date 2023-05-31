9a70f53674f128f7be6262a2f7f3440b114e853c
Joshua Ji <joshua.ji@jiwhiz.com>
2023-05-31 19:53:14 +0000

    Add `train` file

--- azure/pipeline/main.py
+++ azure/pipeline/main.py
@@ -4,24 +4,23 @@
 # relative imports from lib/deploy_helpers.py
 import os
 
+from mldesigner import command_component, Input as MLInput, Output as MLOutput
+from pathlib import Path
+
 import lib.helpers as helper
 from lib import constants
 from lib.prep.main import prep
+from lib.train.main import train
 
 from azure.ai.ml import Input
 from azure.ai.ml.dsl import pipeline
 from azure.ai.ml.constants import AssetTypes
-
-
-from mldesigner import command_component, Input as MLInput, Output as MLOutput
-from pathlib import Path
 
 # Components
 # https://learn.microsoft.com/en-us/azure/machine-learning/how-to-create-component-pipeline-python?view=azureml-api-2#define-component-using-python-function-1
 # Defining them here so Azure can access the `lib/` folder correctly
 @command_component(
     name="prep_data",
-    version="1",
     display_name="Prep Data",
     description="Prepares raw data and provides training, validation and test datasets",
     environment={
@@ -35,9 +34,22 @@ def prepare_data_component(
     val_data: MLOutput(type="uri_folder"),
     test_data: MLOutput(type="uri_folder")
 ):
-    # https://github.com/Azure/azureml-examples/blob/d0d04883ec32508bfff5d455156f86a82b1cdcb7/sdk/python/jobs/pipelines/2e_image_classification_keras_minist_convnet/train/train_component.py
-
     prep(raw_data, train_data, val_data, test_data)
+
+@command_component(
+    name="train_data",
+    display_name="Train Data",
+    description='Trains ML model using training dataset. Saves trained model.',
+    environment={
+        'conda_file': f'{Path(__file__).parent}/lib/train/conda.yaml',
+        'image': 'mcr.microsoft.com/azureml/minimal-ubuntu20.04-py38-cpu-inference',
+    }
+)
+def train_data_component(
+    train_data: MLInput(type="uri_folder"),
+    model_output: MLOutput(type="uri_folder")
+):
+    train(train_data, model_output)
 
 
 # define a pipeline
@@ -49,6 +61,7 @@ def wisconsin_bca_pipeline(input_data):
     E2E ML pipeline using the Wisconsin BCa Dataset
     '''
     prep_node = prepare_data_component(raw_data=input_data)
+    train_node = train_data_component(train_data=prep_node.outputs.train_data)
 
 
 # ref: https://learn.microsoft.com/en-us/azure/machine-learning/how-to-create-component-pipeline-python?view=azureml-api-2
