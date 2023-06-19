ca232156208855d9bdc0a0b91fa79953f1f08a56
Joshua Ji <joshua.ji@jiwhiz.com>
2023-06-19 20:27:58 +0000

    Simplify postcreate script
    `conda init` is no longer giving errors

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -3,18 +3,9 @@
 
 
 # initialize conda in the container
-# somehow this always fails with a `TypeError: memoryview: a bytes-like object is required, not 'str'`
-# but conda is still inittialized. I just ignore the error with `|| true` lol
-# the "yes N" is to skip the interactivity of conda init when it fails
-yes N | conda init || true 
+conda init
 
 # after conda initializes, we can activate the environment
 # this makes sure we have our custom env activated in our new terminals
 # I find this is a better UX than vscode's "python.terminal.activateEnvironment" setting
 echo 'conda activate env' >> ~/.bashrc
-
-# bash colours: https://stackoverflow.com/a/5947802
-# Tell users that the conda error is a-ok!!
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-printf "\nIf there is a conda error, ${GREEN}DO NOT WORRY!! EVERYTHING IS OKAY!!${NC}\nSee .devcontainer/postcreate.sh for more info.\n"
